96eecac49a69c376d903a7d141773fef30e07c9e
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2025-10-17 16:45:16 +0530

    To calculate Bigger Number between three number

---
+++ basics programs/BiggerNumberFrom3.js
@@ -0,0 +1,13 @@
+var a=5
+var b=6
+var c= 7
+console.log("first no is",a)
+console.log("second no is",b)
+console.log("third no is",c)
+if(a>b && a>c){
+    console.log("greater no is",a)
+}else if(b>a && b>c){
+    console.log("greater no is",b)
+}else{
+    console.log("greater no is",c)
+}
