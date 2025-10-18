da16d6238707fa0a10003c6fef753657e6458230
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2025-10-18 19:18:29 +0530

    To calculate Arithmatic Calculation

---
+++ basics programs/ArithmaticCalculation.js
@@ -0,0 +1,14 @@
+var a=10;
+var b=5;
+var sum= a+b
+var sub=a-b
+var mul= a*b
+var d= a/b
+var m= a%b
+console.log("first no",a)
+console.log("second no",b)
+console.log("sum",sum)
+console.log("substract",sub)
+console.log("multiply",mul)
+console.log("divide",d)
+console.log("modulus",m)
