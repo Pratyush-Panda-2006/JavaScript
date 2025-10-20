bcbafa2271f1fadb5dbd61d4d0584ac1cb2b9468
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2025-10-20 18:19:04 +0530

    To calculate the net of an employee

---
+++ basics programs/EmployeeNet.js
@@ -0,0 +1,16 @@
+var b= 20000
+var ta = (4/10)*b
+var da = (8/10)*b
+var hra = (15/100)*b
+var g= b+ ta+ da+ hra
+var lic= (5/100)*g
+var tax=(25/100)*g
+var net= g-(lic+tax)
+console.log("basic is",b)
+console.log("ta is",ta)
+console.log("da is",da)
+console.log("hra is",hra)
+console.log("gross is",g)
+console.log("LIC is",lic)
+console.log("tax is",tax)
+console.log("net is",net)
