d2d3277625442d23d6888321196fc809b5eea725
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2025-10-22 17:20:15 +0530

    To calculate sum of three digits number

---
+++ basics programs/SumOfThreeDigits.js
@@ -0,0 +1,10 @@
+var a=145
+var k= a%10
+var p= a%100
+var n= parseInt(p/10)
+var m= parseInt(a/100)
+var s= k+n+m
+console.log("last digit",k)
+console.log("second digit",n)
+console.log("first digit",m)
+console.log("sum is",s)
